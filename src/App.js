86d5f09e31cbaab3f2a6297be1b7345fb124b843
Michelle Scott <scottmm374@gmail.com>
2019-09-09 18:10:44 -0400

    initial setup, added state property in app.js, deleted uneeded images

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React, { useState } from 'react';
+
+import './App.css';
+
+function App() {
+const [ team, setTeam ] = useState({name: '', email: '', role: ''})
+
+  return (
+    <div className="App">
+      
+    </div>
+  );
+}
+
+export default App;
