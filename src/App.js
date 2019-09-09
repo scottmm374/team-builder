dac1ef8b87e5e055064103602daa8ac3c486be14
Michelle Scott <scottmm374@gmail.com>
2019-09-09 18:24:15 -0400

    added form and handlers

--- src/App.js
+++ src/App.js
@@ -1,13 +1,50 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 
 import './App.css';
 
 function App() {
 const [ team, setTeam ] = useState({name: '', email: '', role: ''})
 
+const handleChange = event => {
+  setTeam({...team, [event.target.name]: event.target.value});
+};
+
+const handleSubmit = event => {
+  event.preventDefault();
+  setTeam({ name: '', email: '', role: ''});
+}
+
   return (
     <div className="App">
-      
+      <form onSubmit={event => handleSubmit(event)}>
+        <label>
+          Name: 
+          <input 
+            type="text"
+            name="name"
+            value={team.name}
+            onChange={event => handleChange(event)}
+            />
+        </label>
+        <label>
+          Email: 
+          <input 
+            type="text"
+            name="email"
+            value={team.email}
+            onChange={event => handleChange(event)}
+            />
+        </label>
+        <label>
+          Role: 
+          <input 
+            type="text"
+            name="role"
+            value={team.role}
+            onChange={event => handleChange(event)}
+            />
+        </label>
+      </form>
     </div>
   );
 }
