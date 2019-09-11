c065b27154f0cd7ee3553897517596acd1532a01
Michelle Scott <scottmm374@gmail.com>
2019-09-10 21:29:27 -0400

    Added routing and navigation

--- src/App.js
+++ src/App.js
@@ -1,7 +1,19 @@
 import React, { useState } from 'react';
+import { Link, Route } from 'react-router-dom';
 import TeamForm from './TeamForm';
 import TeamMemberList from './TeamMemberList';
+import styled from 'styled-components';
 import './App.css';
+
+
+const NavLinks = styled(Link)`
+text-decoration: none;
+color: white;
+font-size: 1.5rem;
+font-weight: bold;
+ margin-right: 30px;
+
+`;
 
 function App() {
   const [team, setTeam] = useState([
@@ -27,8 +39,21 @@ function App() {
 
   return (
     <div>
-      <TeamMemberList team={team} />
-      <TeamForm team={team} setTeam={setTeam} />
+    <nav>
+      <h1 className="nav-header">Team Builder</h1>
+      <div className="nav-links">
+        <NavLinks to='/'>Home</NavLinks>
+        <NavLinks to='/members'>Add Member</NavLinks>
+      </div>
+    </nav>
+    <div>
+      <Route 
+        path="/" exact
+        render={props => <TeamMemberList team={team} {...props} />}/>
+      <Route 
+      path="/members" 
+      render={props => <TeamForm team={team} setTeam={setTeam} {...props} />}/> 
+    </div>
     </div>
   )
 
